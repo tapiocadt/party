eccfc361296d74bfd964be459d7c53ad552bdf29
tapiocadt <tapiocadaterrinha@gmail.com>
2024-08-30 09:33:03 -0300

    feat: link de acesso

--- counter.js
+++ counter.js
@@ -1,15 +1,16 @@
-// The End Of Year Date To Countdown Date
-
 document.addEventListener('DOMContentLoaded', () => {
-    // Unix timestamp (in seconds) to count down to
-    var toDayFromNow = (new Date("Sep 02, 2024 23:59:59").getTime() / 1000) + (3600 / 60 / 60 / 24) - 1;
-    // Set Up FlipDown
-    var flipdown = new FlipDown(toDayFromNow)
 
-    // Start The Count Down
-    .start()
-    // Do Something When The Countdown Ends
-    .ifEnded(() => {
-        document.querySelector(".flipdown").innerHTML = `<h2>O tempo acabou!</h2>`;
-    });
+    var toDayFromNow = (new Date("Sep 02, 2024 23:59:59").getTime() / 1000) + (3600 / 60 / 60 / 24) - 1;
+
+    var flipdown = new FlipDown(toDayFromNow)
+        .start()
+        // .ifEnded(() => {
+        //     document.querySelector(".flipdown").innerHTML = `<h2>O tempo acabou!</h2>`;
+        // });
+
+    const firstRotorGroup = document.querySelectorAll('.rotor-group .rotor-top');
+
+    const daysLeftDiv = document.getElementById('days-left');
+
+    daysLeftDiv.appendChild(firstRotorGroup[1]);
 });
